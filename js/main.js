3fe085398f4668427c0f7fdc30defebda58803dc
Naman Biswajit <62687888+Naman-Biswajit@users.noreply.github.com>
2020-08-11 12:55:57 +0530

    version 0.0.1
    added basic files and main.js for loading file to the app.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log('Main procces working...')
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const { report } = require('process');
+const url = require('url');
+const { protocol } = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../html/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+app.on('ready', createWindow);
