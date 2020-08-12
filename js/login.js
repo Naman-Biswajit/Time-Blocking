dbb45a829b6c06f0241f5c00c6ff905d6b072dd1
Naman Biswajit <62687888+Naman-Biswajit@users.noreply.github.com>
2020-08-12 11:55:11 +0530

    version 0.1.0
    added todo items for planing

---
+++ js/login.js
@@ -0,0 +1,11 @@
+let loggedIn = true;
+
+
+if (loggedIn === true) {
+    console.log('The user is already logged in')
+    window.location.href = "./index.html";
+}
+
+else {
+    console.log('User not logged in')
+}
